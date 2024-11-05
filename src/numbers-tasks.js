8dfb71da5084df40b73afa06195cca5d03455736
qwavy <nursultanr585@gmail.com>
2024-11-05 12:06:11 +0500

    feat: some code

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -196,8 +196,13 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  for(let i = 2;i < n;i++){
+    if(n % i == 0){
+      return true
+    }
+  }
+  return false
 }
 
 /**
@@ -215,8 +220,11 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  if(!isNaN(value)){
+    return value
+  }
+  return def
 }
 
 /**
